169a090a837a0129eaf97e47e52f57154897a145
kenneth Mpemane Mlimi <105229740+Mpemane@users.noreply.github.com>
2023-01-05 03:35:15 +0200

    Delete Say the number.py

--- Say the number.py
+++
@@ -1,7 +0,0 @@
-from num2words import num2words # import the library that is needed for this task.
-num = int(input("Please enter the number that you want to know how  its pronounce: ")) # Prompt the use to enter a number.
-def num_to_words(num): # defines the function.
-    out_put = num2words(num) # initiate a variable output, that outputs the numbers the way it i pronounce
-    print(out_put) # print out the output.
-
-num_to_words(num) # calling the function and outputing the answer.
